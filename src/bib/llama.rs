d3d0a493f224a8258c8ae6e03e997e29eb3b61a9
emmajane1313 <admin@digitalax.xyz>
2024-07-22 07:10:57 -0400

    ollama-rs

--- src/bib/llama.rs
+++ src/bib/llama.rs
@@ -1,11 +1,14 @@
 use crate::bib::types::Llama;
 use ollama_rs::{generation::completion::request::GenerationRequest, Ollama};
-use std::error::Error;
+use std::{error::Error, process::Command};
 
 impl Llama {
     pub async fn llamar_llama(&self, prompt: &str) -> Result<String, Box<dyn Error + Send + Sync>> {
         let ollama = Ollama::new("http://localhost".to_string(), 11434);
-        let model = "llama3:latest".to_string();
+        let model = "llama3:70b".to_string();
+
+        let list_models_output = Command::new("./ollama").arg("list").output()?;
+        println!("lista de los modelos {:?}", list_models_output);
 
         let res = ollama
             .generate(GenerationRequest::new(model, prompt.to_string()))
